5293af4bcd35b26988fc5ea08418b51ff7aa0fef
Ole-Jacob Enderud Jensen <olejacob.jensen@gmail.com>
2025-01-30 22:55:58 +0100

    Changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,23 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        auto_install = true,
+        auto_install = false,
+        ensure_installed = {
+          "bash",
+          "cpp",
+          "c_sharp",
+          "rust",
+          "python",
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "json",
+          "lua",
+        },
         highlight = { enable = true },
-        indent = { enable = true },
+        indent = { enable = false },
       })
     end
   }
